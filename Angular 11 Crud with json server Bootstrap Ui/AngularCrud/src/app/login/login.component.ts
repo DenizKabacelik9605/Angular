0abdc668e50d9d64eb2d6bc71b61e015f6e1f968
Deniz Kabaçelik <deniz.kabacelik@mctek.com.tr>
2023-03-30 08:36:01 +0300

    Login adn Signup

--- Angular 11 Crud with json server Bootstrap Ui/AngularCrud/src/app/login/login.component.ts
+++ Angular 11 Crud with json server Bootstrap Ui/AngularCrud/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import {FormBuilder, FormGroup} from "@angular/forms";
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  public loginForm!: FormGroup
+  constructor(private formBuilder:FormBuilder, private http:HttpClient,private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm=this.formBuilder.group({
+
+      email:[''],
+      password:['']
+    })
   }
 
+  login(){
+
+    this.http.get<any>("http://localhost:3000/signUpUsers").subscribe
+
+
+    ({next:(res)=>{
+
+
+     console.log("res",res[0])
+     const user= res.find((a:any)=>{
+
+       return a.email===this.loginForm.value.email && a.email===this.loginForm.value.password
+     });
+     console.log("login", user);
+     if(user){
+        alert("Login Success");
+          this.loginForm.reset();
+          this.router.navigate(['dashboard'])
+     }else{
+      alert("user not found");
+     }
+    },error:err=>{alert("Something went wrong!!")}
+  })
+  }
 }
